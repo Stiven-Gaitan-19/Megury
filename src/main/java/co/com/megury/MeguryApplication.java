ed7a3e75b69cf70edc499e306b58795811ef9ba2
Johan Stiven Gaitan Peñuela <jsgaitan410@misena.edu.co>
2020-09-24 18:11:02 -0500

    chore: Se agrega proyecto

---
+++ src/main/java/co/com/megury/MeguryApplication.java
@@ -0,0 +1,13 @@
+package co.com.megury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeguryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeguryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/megury/MeguryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.megury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeguryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
