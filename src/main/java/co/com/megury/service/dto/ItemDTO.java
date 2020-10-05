60efe9f90d90fe7e42b4104fd33dc58d7cba8894
Johan Stiven Gaitan Peñuela <jsgaitan410@misena.edu.co>
2020-10-05 11:45:28 -0500

    chore: Se agregan getters and setters a ItemDTO

--- src/main/java/co/com/megury/service/dto/ItemDTO.java
+++ src/main/java/co/com/megury/service/dto/ItemDTO.java
@@ -6,4 +6,28 @@ public class ItemDTO implements Serializable {
 
     private Long id;
     private Long quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDTO{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
